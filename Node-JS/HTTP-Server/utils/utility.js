08abae19ff37c0297639064648aad961d4d8b22a
ayushisharma-05 <ayushi.sharma5205@gmail.com>
2026-08-05 20:00:00 +0530

    Add registration and utility features

---
+++ Node-JS/HTTP-Server/utils/utility.js
@@ -0,0 +1,7 @@
+const STATUS ={
+    SUCESS :200 // (object) jese error codes hote h vo bhi ese set karte h and puri application me same value set hoti h 
+}
+const TYPE = {
+    'content-type':'text/html' // object respnse udar bhejenge and uska type text html type ka hga to browser ready rehegs ki html content ayega usme ! 
+}
+module.exports = {STATUS,TYPE} // named export
