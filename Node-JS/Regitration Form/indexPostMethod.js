39f33ea835cf3c72f801338f350342446ead3aaf
ayushisharma-05 <ayushi.sharma5205@gmail.com>
2026-08-10 20:00:00 +0530

    Add Node.js registration form

---
+++ Node-JS/Regitration Form/indexPostMethod.js
@@ -0,0 +1,63 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var qs = require("querystring");
+
+var instance = http.createServer((request, response) => {
+    response.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    var requestedURL = url.parse(request.url).pathname;
+
+    if ((requestedURL == "/" || requestedURL == "/home") ) {
+        fs.readFile("Home1.html",(error,data)=>{
+            if(error)
+                console.log("error Occured");
+            else{
+                response.write(data);
+                response.end();
+            }
+        })
+    }
+    else if ((requestedURL == "/register" ) ) {
+        fs.readFile("registerPost.html",(error,data)=>{
+            if(error)
+                console.log("error Occured");
+            else{
+                response.write(data);
+                response.end();
+            }
+        })
+    }
+    else if ((requestedURL == "/viewDetails" ) ) {
+        // var data = url.parse(request.url,true).query;
+        // response.write("Username : "+data.username+"<br>");
+        // response.write("Email : "+data.email+"<br>");
+        // response.write("Password : "+data.password+"<br>");
+        // response.write("Address : "+data.address+"<br>");
+        // response.write("<a href='/home'style='background-color: red;'>Go back</a>")
+        var chunkData = '';
+        request.on('data',(chunk)=>{
+            chunkData+=chunk;
+        })
+        request.on('end',()=>{
+            var data = qs.parse(chunkData);
+            response.write("Username : "+data.username+"<br>");
+            response.write("Email : "+data.email+"<br>");
+            response.write("Password : "+data.password+"<br>");
+            response.write("Address : "+data.address+"<br>");
+            response.write("<a href='/home'style='background-color: red;'>Go back</a>")
+             response.end(); 
+        })
+       
+    }
+    else {
+        response.write("<h1>404 - Page Not Found</h1>");
+        response.end();
+    }
+    
+});
+
+instance.listen(3000, () => {
+    console.log(`Server connection established`);
+});
